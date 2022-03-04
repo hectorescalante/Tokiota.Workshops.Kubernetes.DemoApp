4a8e7e7723f20b8e320dbfb9ffb0fb9f3f5aa797
Hector Escalante <hector.escalante@tokiota.com>
2022-03-04 23:08:15 +0100

    fix route conflict

--- src/ClientApp/src/App.js
+++ src/ClientApp/src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/characters' component={FetchData} />
+        <Route path='/character-list' component={FetchData} />
       </Layout>
     );
   }
--- src/ClientApp/src/components/NavMenu.js
+++ src/ClientApp/src/components/NavMenu.js
@@ -34,7 +34,7 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/characters">Characters</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/character-list">Characters</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
